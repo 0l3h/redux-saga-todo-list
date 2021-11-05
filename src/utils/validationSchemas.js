52f8b6be981eeca74ecda12c9c02fa8323f23bcb
0l3h <kubik7950@gmail.com>
2021-11-05 13:37:39 +0200

    Changed error messages and validation

--- src/utils/validationSchemas.js
+++ src/utils/validationSchemas.js
@@ -1,7 +1,7 @@
 import * as yup from 'yup';
 
 const validationSchema = yup.object({
-    body: yup.string().required(),
+    body: yup.string().max(100, "Text must not exceed length in 100 characters").required("Text must be at least 1 character"),
 })
 
 export default validationSchema;
