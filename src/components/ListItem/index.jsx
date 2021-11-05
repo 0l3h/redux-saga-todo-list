a1d4c65a99bf47ecac10692fe75fabc49268f997
0l3h <kubik7950@gmail.com>
2021-11-05 13:34:51 +0200

    Added proptypes

--- src/components/ListItem/index.jsx
+++ src/components/ListItem/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './ListItem.module.sass';
 
 function ListItem(props) {
@@ -12,6 +13,13 @@ function ListItem(props) {
             <button className={styles.doneButton} onClick={deleteTask}>done</button>
         </section>
     )
+}
+
+ListItem.propTypes = {
+    task: PropTypes.shape({
+        body: PropTypes.string.isRequired,
+    }),
+    deleteTask: PropTypes.func.isRequired,
 }
 
 export default ListItem;
